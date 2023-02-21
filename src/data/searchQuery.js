966a9b67f1a7c34b0341622aadaf3887e2f97e44
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-02-21 14:59:40 +0500

    Add files via upload
    Data is going to card component

--- src/data/searchQuery.js
+++ src/data/searchQuery.js
@@ -1,17 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import CardLayout from "../components/card";
 const FetchData = (props) => {
     const url='https://api.github.com/'+props;
-   return axios.get(url)
-          .then((response) => {
-            //const data = response.data
-            console.log(response.data)
-            CardLayout(response.data);
-            console.log(response.data.followers);
-            
-});
-
+    
+   return axios.get(url);
+    
   }
  
 export default FetchData;
