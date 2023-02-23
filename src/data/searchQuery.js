a428e17b633025cea3d1b54f729ed60bbef0995d
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-02-23 17:44:09 +0500

    Add files via upload

--- src/data/searchQuery.js
+++ src/data/searchQuery.js
@@ -1,17 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import CardLayout from "../components/card";
+import CardLayout from "../components/CardLayout";
 const FetchData = (props) => {
-    const url='https://api.github.com/'+props;
-   return axios.get(url)
-          .then((response) => {
-            //const data = response.data
-            console.log(response.data)
-            CardLayout(response.data);
-            console.log(response.data.followers);
-            
-});
-
+    const url='https://api.github.com/search/'+props;
+    const url1='https://api.github.com/orgs/ORG/'+props+'?per_page=2';
+   return axios.get(url);
+    
   }
  
 export default FetchData;
