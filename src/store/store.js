eea434bc65947a55d174400754fdbfaa06f1986a
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-02-27 19:56:53 +0500

    Add files via upload

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import resultReducer from './resultSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import urlReducer from './url-slice.js';
+const searchSlice = createSlice({
+  name: 'search',
+  initialState: { searchInput: '', dropdownValue: 'users' },
+  reducers: {
+    setSearch: (state, action) => {
+      state.searchInput = action.payload;
+    },
+    setDropdown: (state, action) => {
+      state.dropdownValue = action.payload;
+    },
+  },
+});
 
+const store = configureStore({
+  reducer: {
+    result: resultReducer,
+    search: searchSlice.reducer,
+    url: urlReducer,
+  },
+});
+
+export const { setSearch, setDropdown } = searchSlice.actions;
+
+export default store;
