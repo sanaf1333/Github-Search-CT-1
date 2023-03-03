347e4ace7d45bc61d754055daf511c2373948126
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-03 20:16:46 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HomePage from "./pages/home-page.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
