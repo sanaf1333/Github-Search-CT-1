ae98c0ab2db2dcb444f5e69643892913283359a3
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-03 20:14:45 +0500

    Add files via upload

---
+++ src/shared-components/nav-bar/top-bar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { GithubFilled } from "@ant-design/icons";
+import { Space } from "antd";
+const TopBar = (props) => {
+  var text = " Github Searcher \n hello";
+  return (
+    <Space align="start">
+      <GithubFilled style={{ fontSize: "46px", color: props }} />
+
+      <div style={{ whiteSpace: "pre", color: props }}>
+        <div
+          style={{ fontFamily: "fantasy", fontSize: "20px", paddingTop: "6px" }}
+        >
+          Github Search
+        </div>
+        <div>Search users, repositories and issues</div>
+      </div>
+    </Space>
+  );
+};
+
+export default TopBar;
