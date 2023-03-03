732d599eee3e43020b85ba26b004d39bd0e32a9c
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-03 20:06:12 +0500

    Fixed/fetchProducts-results concatenation
    Added results concatenation

--- src/redux-store/reducers/result-slice.jsx
+++ src/redux-store/reducers/result-slice.jsx
@@ -3,10 +3,12 @@ const {
   combineReducers,
   createAsyncThunk,
 } = require("@reduxjs/toolkit");
+import { useSelector } from "react-redux";
 export const STATUSES = Object.freeze({
   IDLE: "idle",
   ERROR: "error",
   LOADING: "loading",
+  FINISH: "finish",
 });
 
 const ResultSlice = createSlice({
@@ -14,6 +16,9 @@ const ResultSlice = createSlice({
   initialState: {
     data: [],
     status: STATUSES.IDLE,
+    pageNumber: 0,
+    URL: "",
+    resultCount: 0,
   },
   reducers: {
     setResults(state, action) {
@@ -25,47 +30,67 @@ const ResultSlice = createSlice({
     clearResults(state) {
       state.data = [];
     },
+    setPageNumber(state,action){
+      state.pageNumber=action.payload;
+      //console.log("set page num in redux ",action.payload);
+    },
+    setURL(state,action){
+      state.URL=action.payload;
+      console.log("seturl");
+      console.log(state.URL);
+      console.log(action.payload);
+    },
+    setResultCount(state,action){
+      state.resultCount=action.payload;
+    }
   },
+  
 });
 
-export const { setResults, setStatus, clearResults } = ResultSlice.actions;
+export const { setResults, setStatus, clearResults, setPageNumber, setURL, setResultCount } = ResultSlice.actions;
 export default ResultSlice.reducer;
 
 export function fetchProducts(props) {
+  console.log("fetch");
+  console.log(props);
   return async function fetchProductThunk(dispatch, getState) {
+    const pageNumber = getState().result.pageNumber;
+    const URL= getState().result.URL;
+    const currentResults = getState().result.data;
+    console.log("URL from Redux store: ", getState().result.URL);
+    console.log("props");
+    console.log(props);
     dispatch(setStatus(STATUSES.LOADING));
     try {
       const headers = { Accept: "application/vnd.github.text-match+json" };
       const res = await fetch(
         "https://api.github.com/search/" +
-          props +
-          "&per_page=100&media=text-match",
+          URL +
+          "&per_page=20&page="+pageNumber,
         { headers }
       );
       const data = await res.json();
-      console.log(res);
-      console.log("data");
-      console.log(data);
-
-      // Extract metadata for each user
-      const usersWithMetadata = data.items.map((user) => {
-        const { login, score, ...rest } = user;
-        const metadata = rest.text_matches
-          ? rest.text_matches.reduce((acc, match) => {
-              acc[match.property] = match.fragment;
-              return acc;
-            }, {})
-          : {};
-        return { login, score, metadata };
-      });
-
-      console.log(usersWithMetadata);
-
-      dispatch(setResults(data.items));
+      
+      dispatch(setResultCount(data.total_count));
+      if(pageNumber>1){
+        const newResults = data.items;
+        const allResults = concatResults(currentResults, newResults);
+        dispatch(setResults(allResults));
+      }
+      else{
+        dispatch(setResults(data.items));
+      }
       dispatch(setStatus(STATUSES.IDLE));
+      if(pageNumber===1 && data.total_count===0){
+        dispatch(setStatus(STATUSES.ERROR));
+      }
     } catch (err) {
       console.log(err);
       dispatch(setStatus(STATUSES.ERROR));
     }
   };
 }
+
+export const concatResults = (results, newResults) => {
+  return [...results, ...newResults];
+};
