9c05b367cd12e2d49cd862d5a19ce46a3a31e138
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-06 15:18:58 +0500

    Fixed/removed fetchData

--- src/hooks/use-infinite-scroll-hook.jsx
+++ src/hooks/use-infinite-scroll-hook.jsx
@@ -1,35 +1,32 @@
-import { useState, useRef, useCallback, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setPageNumber } from '../redux-store/reducers/result-slice.jsx';
-import { fetchProducts } from '../redux-store/reducers/result-slice.jsx';
-function useInfiniteScroll() {
-  
-  const { data: results, status, pageNumber, URL, resultCount } = useSelector((state) => state.result);
-  
-  const dispatch = useDispatch();
-  const fetchData = async () => {
-    console.log("fetchdata");
-    dispatch(setPageNumber(pageNumber + 1));
-    await dispatch(fetchProducts(URL));
-  };
+import { useState, useRef, useCallback, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+function useInfiniteScroll(fetchData) {
+  const {
+    data: results,
+    status,
+    pageNumber,
+    URL,
+    resultCount,
+  } = useSelector((state) => state.result);
+
   const loadMoreRef = useRef(null);
-  const [isInter,setIsInter]=useState(false);
+  const [isInter, setIsInter] = useState(false);
   const handleObserver = useCallback((entries) => {
     const [target] = entries;
-    if (target.isIntersecting && pageNumber>0 && results.length<resultCount) {
-      console.log("target is intersecting");
-      console.log(status);
+    if (
+      target.isIntersecting &&
+      pageNumber > 0 &&
+      results.length < resultCount
+    ) {
       setIsInter(true);
       fetchData();
     }
-    
-    
   });
 
   useEffect(() => {
     const option = {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 1.0,
     };
 
