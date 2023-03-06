b27aaed05039e43d9bd5bc380d128a1d30898398
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-06 15:19:52 +0500

    Fixed/removed navbar

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,49 +1,11 @@
-import React, { useState } from "react";
-import { Provider } from "react-redux";
-import { ConfigProvider, Switch, Layout, Typography } from "antd";
-import TopBar from "../components/navbar/top-bar.jsx";
-import SearchAndDropdown from "../components/search/search-and-dropdown.jsx";
-import store from "../redux-store/store/store.jsx";
-import * as lightTheme from "../ant-tokens/light.json";
-import * as darkTheme from "../ant-tokens/dark.json";
-import { BulbOutlined, BulbFilled } from "@ant-design/icons";
-
+import React from "react";
+import { Layout } from "antd";
+import SearchAndDropdownContainer from "../containers/search/search-and-dropdown-container.jsx";
 const HomePage = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-  const icon = isDarkMode ? <BulbOutlined /> : <BulbFilled />;
-  const handleToggleDarkMode = (checked) => {
-    setIsDarkMode(checked);
-  };
-
   return (
-    <ConfigProvider
-      theme={{
-        token: isDarkMode ? darkTheme : lightTheme,
-      }}
-    >
-      <Provider store={store}>
-        <Layout style={{ minHeight: "100vh" }}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "10px",
-            }}
-          >
-            {TopBar(isDarkMode ? "white" : "black")}
-            <Switch
-              checkedChildren={icon}
-              unCheckedChildren={icon}
-              defaultChecked
-              onChange={handleToggleDarkMode}
-            />
-          </div>
-          <Layout style={{ padding: "20px" }}>
-            <SearchAndDropdown />
-          </Layout>
-        </Layout>
-      </Provider>
-    </ConfigProvider>
+    <Layout style={{ padding: "20px" }}>
+      <SearchAndDropdownContainer />
+    </Layout>
   );
 };
 
