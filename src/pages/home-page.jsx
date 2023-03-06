f82db4d683e17c7f65506673dd757aa485706fbe
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-06 17:51:41 +0500

    Added absolute imports

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "antd";
-import SearchAndDropdownContainer from "../containers/search/search-and-dropdown-container.jsx";
+import SearchAndDropdownContainer from "containers/search/search-and-dropdown-container.jsx";
 const HomePage = () => {
   return (
     <Layout style={{ padding: "20px" }}>
