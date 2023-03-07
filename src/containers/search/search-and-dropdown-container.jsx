f6555f8551e661c3b6d39756d89cd33b57b37f15
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-07 18:55:00 +0500

    Add files via upload

--- src/containers/search/search-and-dropdown-container.jsx
+++ src/containers/search/search-and-dropdown-container.jsx
@@ -1,1 +1,42 @@
+import React, { useState } from "react";
+import useSearchHandler from "@hooks/use-search-handler-hook.js";
+import SearchAndDropdown from "@components/search/search-and-dropdown.jsx";
+import { useSelector } from "react-redux";
 
+const SearchAndDropdownContainer = React.memo(() => {
+  const [searchInput, setSearchInput] = useState("");
+  const [dropdownValue, setDropdownValue] = useState("users");
+  const { handleSearchInput, handleSearchDropdown } = useSearchHandler();
+  const { data: results } = useSelector((state) => state.result);
+  
+  function handleChangeDropdown(value) {
+    setDropdownValue(value);
+    handleSearchDropdown(value, searchInput);
+  }
+
+  function handleChangeSearchInput(e) {
+    setSearchInput(e);
+    handleSearchInput(e, dropdownValue);
+  }
+
+  return (
+    <SearchAndDropdown
+      searchInput={searchInput}
+      dropdownValue={dropdownValue}
+      handleChangeSearchInput={handleChangeSearchInput}
+      handleChangeDropdown={handleChangeDropdown}
+      results={results}
+    />
+  );
+}, (prevProps, nextProps) => {
+  console.log("previosu props",prevProps);
+  console.log(nextProps);
+  return (
+  
+    prevProps.searchInput === nextProps.searchInput &&
+    prevProps.dropdownValue === nextProps.dropdownValue &&
+    prevProps.results === nextProps.results
+  );
+});
+
+export default SearchAndDropdownContainer;
