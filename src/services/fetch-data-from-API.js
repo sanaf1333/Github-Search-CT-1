07fb0cbcade315a0162a6273380815b2a70ce8ab
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-07 19:05:19 +0500

    Add files via upload

--- src/services/fetch-data-from-API.js
+++ src/services/fetch-data-from-API.js
@@ -1,1 +1,15 @@
+const API_URL = process.env.REACT_APP_API_URL;
 
+export default async function fetchDataFromAPI(
+  searchKeyword,
+  pageCount,
+  pageNumber
+) {
+  const headers = { Accept: "application/vnd.github.text-match+json" };
+  const res = await fetch(
+    `${API_URL}${searchKeyword}&per_page=${pageCount}&page=${pageNumber}`,
+    { headers }
+  );
+  const data = await res.json();
+  return data;
+}
