b26a9709866141ee9a7094c306ff10c1304c181f
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-07 19:00:19 +0500

    Add files via upload

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { Layout } from "antd";
 import SearchAndDropdownContainer from "containers/search/search-and-dropdown-container.jsx";
 const HomePage = () => {
   return (
-    <Layout style={{ padding: "20px" }}>
       <SearchAndDropdownContainer />
-    </Layout>
   );
 };
 
