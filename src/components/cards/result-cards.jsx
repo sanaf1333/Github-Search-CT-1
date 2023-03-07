3eda57e2dca0105def6a9c09725dec12b015844a
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-07 18:49:39 +0500

    Fixed/converted to pure component

--- src/components/cards/result-cards.jsx
+++ src/components/cards/result-cards.jsx
@@ -1,73 +1,42 @@
-import React, { useRef, useEffect, useState, memo } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { ConfigProvider, Row, Col } from "antd";
-import { Typography } from "antd";
-import DestructAPIResponse from "helpers/destruct-api-response.js";
+import React from "react";
+import { Row, Col, Typography, Layout } from "antd";
+import DestructAPIResponse from "@helpers/destruct-api-response.js";
 import LoadingCardSkeleton from "./loading-card-skeleton.jsx";
-import CardLayout from "./card-skeleton/card-layout-user.jsx";
-import CardLayoutIssue from "./card-skeleton/card-layout-issue.jsx";
-import CardLayoutRepos from "./card-skeleton/card-layout-repos.jsx";
-import useInfiniteScroll from "hooks/use-infinite-scroll-hook.jsx";
-import {
-  setPageNumber,
-  fetchProducts,
-  STATUSES,
-} from "../../redux-store/reducers/result-slice.jsx";
-const { Title, Text } = Typography;
-const ResultCards = () => {
-  const {
-    data: results,
-    status,
-    pageNumber,
-    URL,
-    resultCount,
-  } = useSelector((state) => state.result);
-
-  const dropdownValue = useSelector((state) => state.search.dropdownValue);
-  const dispatch = useDispatch();
-
-  const fetchData = async () => {
-    console.log("fetchdata");
-    dispatch(setPageNumber(pageNumber + 1));
-    dispatch(fetchProducts());
-  };
-  const { loadMoreRef, isInter } = useInfiniteScroll(fetchData);
-
-  const cardLayoutMap = {
-    repositories: CardLayoutRepos,
-    issues: CardLayoutIssue,
-    default: CardLayout,
-  };
-
-  if (status === STATUSES.ERROR) {
-    return <Title level={4}>Oops! Something went wrong.</Title>;
-  }
-
-  const CardLayoutComponent =
-    cardLayoutMap[dropdownValue] || cardLayoutMap.default;
-
+import { STATUSES } from "@redux-store/reducers/get-API-results.js";
+const { Text } = Typography;
+const ResultCards = ({
+  results,
+  CardLayoutComponent,
+  resultCount,
+  loadMoreRef,
+  dropdownValue,
+  status,
+  pageNumber,
+}) => {
   return (
-    <div>
-      {
-        <div>
-          <Row gutter={[10, 10]} justify="center">
-            {results.map((cardData) => (
-              <div style={{ padding: "25px" }} key={cardData.id}>
-                <Col xs={12} sm={12} md={8} lg={6}>
-                  <CardLayoutComponent
-                    {...DestructAPIResponse(cardData, dropdownValue)}
-                  />
-                </Col>
-              </div>
-            ))}
-          </Row>
-        </div>
-      }
-      {status != "loading" && <div ref={loadMoreRef}></div>}
+    <Layout>  
+      <Row gutter={[10, 10]} justify="center">
+        {results.map((cardData) => (
+          <Layout style={{ padding: "25px" }} key={cardData.id}>
+            <Col xs={12} sm={12} md={8} lg={6}>
+              <CardLayoutComponent
+                {...DestructAPIResponse(cardData, dropdownValue)}
+              />
+            </Col>
+          </Layout>
+        ))}
+      </Row>
+      {status !== "loading" && <div ref={loadMoreRef}></div>}
+      {status === STATUSES.LOADING && <LoadingCardSkeleton />}
+      {status === STATUSES.SUCCESS && <Text strong>NO RESULTS FOUND</Text>}
+      {status != STATUSES.LOADING && <div ref={loadMoreRef}></div>}
       {results.length > 0 && results.length === resultCount && (
         <Text strong>NO MORE RESULTS</Text>
       )}
-    </div>
+      {pageNumber > 1 && status === STATUSES.ERROR && (
+        <Text strong>OOPS! Something went wrong.</Text>
+      )}
+    </Layout>
   );
 };
 
