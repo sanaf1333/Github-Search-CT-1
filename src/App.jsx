9ecb8bb7611b5950117c86152b9505a81eb53580
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-07 18:45:06 +0500

    Fixed/Added theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,20 @@
-import HomePage from "./pages/home-page.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React, { useState } from "react";
+import React from "react";
 import { Provider } from "react-redux";
-import { ConfigProvider, Switch, Layout } from "antd";
-import TopBar from "shared-components/navbar/top-bar.jsx";
-import store from "redux-store/store/store.jsx";
-import * as lightTheme from "ant-tokens/light.json";
-import * as darkTheme from "ant-tokens/dark.json";
-import { BulbOutlined, BulbFilled } from "@ant-design/icons";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "@pages/home-page.jsx";
+import store from "@redux-store/store/store.js";
+import AppLayout from "@styles/app-layout.js";
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-  const icon = isDarkMode ? <BulbOutlined /> : <BulbFilled />;
-  const handleToggleDarkMode = (checked) => {
-    setIsDarkMode(checked);
-  };
   return (
-    <ConfigProvider
-      theme={{
-        token: isDarkMode ? darkTheme : lightTheme,
-      }}
-    >
-      <Provider store={store}>
-        <Layout style={{ minHeight: "100vh" }}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "10px",
-            }}
-          >
-            {TopBar(isDarkMode ? "white" : "black")}
-            <Switch
-              checkedChildren={icon}
-              unCheckedChildren={icon}
-              defaultChecked
-              onChange={handleToggleDarkMode}
-            />
-          </div>
-          <Router>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-            </Routes>
-          </Router>
-        </Layout>
-      </Provider>
-    </ConfigProvider>
+    <Provider store={store}>
+      <AppLayout>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Router>
+      </AppLayout>
+    </Provider>
   );
 };
 
