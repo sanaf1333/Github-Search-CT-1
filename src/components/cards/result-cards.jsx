4a1d440fb025ca62b0c56dc832d916be98db2f04
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-09 19:44:56 +0500

    Fixed/div to antd

--- src/components/cards/result-cards.jsx
+++ src/components/cards/result-cards.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Row, Col, Typography, Layout } from "antd";
-import DestructAPIResponse from "@helpers/destruct-api-response.js";
+import destructAPIResponse from "@helpers/destruct-api-response.js";
 import LoadingCardSkeleton from "./loading-card-skeleton.jsx";
-import { STATUSES } from "@redux-store/reducers/get-API-results.js";
+import { STATUSES } from "@redux-store/reducers/API-results-slice";
 const { Text } = Typography;
 const ResultCards = ({
   results,
@@ -15,25 +15,25 @@ const ResultCards = ({
 }) => {
   return (
     <Layout>  
-      <Row gutter={[10, 10]} justify="center">
+      { results && <Row gutter={[10, 10]} justify="center">
         {results.map((cardData) => (
           <Layout style={{ padding: "25px" }} key={cardData.id}>
             <Col xs={12} sm={12} md={8} lg={6}>
               <CardLayoutComponent
-                {...DestructAPIResponse(cardData, dropdownValue)}
+                {...destructAPIResponse(cardData, dropdownValue)}
               />
             </Col>
           </Layout>
         ))}
-      </Row>
-      {status !== "loading" && <div ref={loadMoreRef}></div>}
+      </Row>}
       {status === STATUSES.LOADING && <LoadingCardSkeleton />}
+      {status !== "loading" && <div ref={loadMoreRef}></div>}     
       {status === STATUSES.SUCCESS && <Text strong>NO RESULTS FOUND</Text>}
       {status != STATUSES.LOADING && <div ref={loadMoreRef}></div>}
-      {results.length > 0 && results.length === resultCount && (
+      {results && results.length > 0 && results.length === resultCount && (
         <Text strong>NO MORE RESULTS</Text>
       )}
-      {pageNumber > 1 && status === STATUSES.ERROR && (
+      { status === STATUSES.ERROR && (
         <Text strong>OOPS! Something went wrong.</Text>
       )}
     </Layout>
