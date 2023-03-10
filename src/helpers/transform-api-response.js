cfe3d81eb7209f284a6d36cd312ac5ab3f792116
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-10 16:30:11 +0500

    update/function name

---
+++ src/helpers/transform-api-response.js
@@ -0,0 +1,36 @@
+import extractDate from "./format-date";
+
+export default function transformAPIResponse(data, dropdownValue){
+    let cardProps={};
+    if(dropdownValue==="users"){
+        cardProps = {
+            login: data.login,
+            followersURL: data.followers_url,
+            followingURL: data.following_url,
+            avatarURL: data.avatar_url,
+            url: data.url,
+            htmlURL: data.html_url,
+          };
+    }
+    if(dropdownValue==="repositories"){
+        cardProps = {
+            fullName: data.full_name,
+            createdAt: extractDate(data.created_at),
+            forksCount: data.forks_count,
+            language: data.language || "N/A",
+            htmlURL: data.html_url,
+            watchersCount: data.watchers_count,
+          };
+    }
+    if(dropdownValue==="issues"){
+        cardProps = {
+            title: data.title,
+            createdAt: extractDate(data.created_at),
+            updatedAt: extractDate(data.updated_at),
+            comments: data.comments,
+            htmlURL: data.html_url,
+            state: data.state,
+          };
+    }
+    return cardProps;
+}
