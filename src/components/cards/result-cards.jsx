d0eec6b9a6a8d1361c197834dc7f1eca20078476
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-10 16:24:20 +0500

    fixed/selectors

--- src/components/cards/result-cards.jsx
+++ src/components/cards/result-cards.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Row, Col, Typography, Layout } from "antd";
-import destructAPIResponse from "@helpers/destruct-api-response.js";
+import transformAPIResponse from "@helpers/transform-api-response.js";
 import LoadingCardSkeleton from "./loading-card-skeleton.jsx";
-import { STATUSES } from "@redux-store/reducers/API-results-slice";
+import { STATUSES } from "@redux-store/reducers/github-search-API-slice";
 const { Text } = Typography;
 const ResultCards = ({
   results,
@@ -11,29 +11,30 @@ const ResultCards = ({
   loadMoreRef,
   dropdownValue,
   status,
-  pageNumber,
 }) => {
   return (
-    <Layout>  
-      { results && <Row gutter={[10, 10]} justify="center">
-        {results.map((cardData) => (
-          <Layout style={{ padding: "25px" }} key={cardData.id}>
-            <Col xs={12} sm={12} md={8} lg={6}>
-              <CardLayoutComponent
-                {...destructAPIResponse(cardData, dropdownValue)}
-              />
-            </Col>
-          </Layout>
-        ))}
-      </Row>}
+    <Layout>
+      {results && (
+        <Row gutter={[10, 10]} justify="center">
+          {results.map((cardData) => (
+            <Layout style={{ padding: "25px" }} key={cardData.id}>
+              <Col xs={12} sm={12} md={8} lg={6}>
+                <CardLayoutComponent
+                  {...transformAPIResponse(cardData, dropdownValue)}
+                />
+              </Col>
+            </Layout>
+          ))}
+        </Row>
+      )}
       {status === STATUSES.LOADING && <LoadingCardSkeleton />}
-      {status !== "loading" && <div ref={loadMoreRef}></div>}     
+      {status != STATUSES.LOADING && <div ref={loadMoreRef}></div>}
       {status === STATUSES.SUCCESS && <Text strong>NO RESULTS FOUND</Text>}
       {status != STATUSES.LOADING && <div ref={loadMoreRef}></div>}
       {results && results.length > 0 && results.length === resultCount && (
         <Text strong>NO MORE RESULTS</Text>
       )}
-      { status === STATUSES.ERROR && (
+      {status === STATUSES.ERROR && (
         <Text strong>OOPS! Something went wrong.</Text>
       )}
     </Layout>
