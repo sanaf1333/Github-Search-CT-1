eb9fefb96c8c753634bdc8dbeded6110887c0ea1
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-03-10 16:26:06 +0500

    fixed/useSelector

--- src/components/search/search-and-dropdown.jsx
+++ src/components/search/search-and-dropdown.jsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Input, Select, Space, Layout } from "antd";
+import { Input, Select, Space, Layout, Col } from "antd";
 import ResultCardsContainer from "@containers/cards/result-cards-container.jsx";
-const SearchAndDropdown = (props) => {
-  const {
-    searchInput,
-    dropdownValue,
-    handleChangeSearchInput,
-    handleChangeDropdown,
-    results,
-  } = props;
+const SearchAndDropdown = ({
+  searchInput,
+  dropdownValue,
+  handleChangeSearchInput,
+  handleChangeDropdown,
+  results,
+}) => {
   return (
     <Layout
       style={{
-        //display: "flex",
-        //flexDirection: "column",
         alignItems: "center",
         paddingTop: results && results.length > 0 ? "0px" : "100px",
       }}
     >
-      <Layout style={{ marginBottom: "20px" }}>
+      <Col style={{ marginBottom: "20px" }}>
         <Space direction="horizontal">
           <Input
             placeholder="Enter keywords"
@@ -49,7 +46,7 @@ const SearchAndDropdown = (props) => {
             ]}
           />
         </Space>
-      </Layout>
+      </Col>
       <ResultCardsContainer />
     </Layout>
   );
